156ef5e5523b0464c45dd6c7633fc5e3f35ff3f0
dmiante <dmi4ndev@gmail.com>
2023-12-28 01:02:09 -0300

    add: dashboard page component

--- src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,13 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'app-dashboard-layout',
-  standalone: true,
-  imports: [
-    CommonModule,
-  ],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.css',
 })
-export class DashboardLayoutComponent { }
+export class DashboardLayoutComponent {
+  private _authService = inject(AuthService)
+
+  public user = computed(() => this._authService.currentUser())
+  // get user(){
+  //   return this._authService.currentUser()
+  // }
+}
