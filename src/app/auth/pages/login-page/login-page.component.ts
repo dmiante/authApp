501b130cb6684587fa0777e4fd0361e5d17e5ebf
dmiante <dmi4ndev@gmail.com>
2023-12-27 22:23:49 -0300

    add: layout login page

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,13 +1,20 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  standalone: true,
-  imports: [
-    CommonModule,
-  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css',
 })
-export class LoginPageComponent { }
+export class LoginPageComponent {
+  private fb = inject(FormBuilder)
+
+  public myForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  })
+
+  login(){
+    console.log(this.myForm.value);
+  }
+}
