78501890b34c18573ba039c16b1dc7fcbffbc7a4
dmiante <dmi4ndev@gmail.com>
2023-12-27 21:59:02 -0300

    add: routes and routes children

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    // guards
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'dashboard',
+    // guards
+    loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'auth'
+  }
+];
