fa3c9774e007d6ab266196bfeb0e24ab72c30553
dmiante <dmi4ndev@gmail.com>
2023-12-28 02:16:07 -0300

    add: guard when is not authenticated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { isAuthenticatedGuard } from './auth/guards/is-authenticated.guard';
+import { isNotAuthenticatedGuard } from './auth/guards/is-not-authenticated.guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
-    // guards
+    canActivate: [isNotAuthenticatedGuard],
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
   {
