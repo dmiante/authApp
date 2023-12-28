752d133f0ce0441e5fcc7faee7b5095d060d7144
dmiante <dmi4ndev@gmail.com>
2023-12-28 01:02:39 -0300

    add: guard for user authenticated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { isAuthenticatedGuard } from './auth/guards/is-authenticated.guard';
 
 export const routes: Routes = [
   {
@@ -8,7 +9,7 @@ export const routes: Routes = [
   },
   {
     path: 'dashboard',
-    // guards
+    canActivate: [isAuthenticatedGuard],
     loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
   },
   {
