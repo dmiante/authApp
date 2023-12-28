82cb6c22e560f9526a79bb132b778ca94854f6b8
dmiante <dmi4ndev@gmail.com>
2023-12-28 01:03:26 -0300

    add: navigate to dashboard from login

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { AuthService } from '../../services/auth.service';
 import Swal from 'sweetalert2';
 
@@ -11,17 +13,18 @@ import Swal from 'sweetalert2';
 export class LoginPageComponent {
   private fb = inject(FormBuilder)
   private authService = inject(AuthService)
+  private router = inject(Router)
 
   public myForm: FormGroup = this.fb.group({
     email: ['damian@google.cl', [Validators.required, Validators.email]],
-    password: ['1234567', [Validators.required, Validators.minLength(6)]]
+    password: ['123456', [Validators.required, Validators.minLength(6)]]
   })
 
   login(){
     const {email, password} = this.myForm.value
     this.authService.login(email, password)
     .subscribe({
-      next: () => console.log('Todo bien'),
+      next: () => this.router.navigateByUrl('/dashboard'),
       error: (msg) => {
         Swal.fire('Error', msg, 'error')
       }
